13785d94d6de23753e2722638cae02fa2caa843b
Wilson Neto <contato@wilsonneto.com.br>
2021-10-26 15:13:14 -0300

    sending files

---
+++ HelloWorldAPI/CreatorsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldAPI
+{
+    class SocialData
+    {
+        public string Social { get; set; }
+        public string Link { get; set; }
+    }
+    class AuthorCard
+    {
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public List<string> tags { get; set; }
+        public List<SocialData> Socials { get; set; }
+        public string Photo { get; set; }
+        public bool Online { get; set; }
+    }
+
+    [ApiController]
+    [Route("/api/hello")]
+    public class CreatorsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Hello()
+        {
+            var Renato = new AuthorCard()
+            {
+                DisplayName = "renatogroffe",
+                Description = "Microsoft Most Valuable Professional (MVP), Multi-Plataform Technical Audience Contributor (MTAC), Software Engineer, Technical Writer and Speaker",
+                Online = true,
+                Photo = "https://avatars.githubusercontent.com/u/8309296?v=4",
+                tags = new List<string>() { ".Net", "Azure", "DevOps", "Tudo mais..." }
+            };
+
+            var Milton = new AuthorCard()
+            {
+                DisplayName = "miltoncamara",
+                Description = "Microsoft MVP, MCP & MCSA, Community Leader & Speaker, Cloud & DevOps Consultan",
+                Online = true,
+                Photo = "https://avatars.githubusercontent.com/u/382444?v=4",
+                tags = new List<string>() { ".Net", "Azure", "DevOps", "Tudo mais..." }
+            };
+
+            return Ok(new List<AuthorCard>() { 
+                Renato, Milton
+            });
+        }
+    }
+}
---
+++ HelloWorldAPI/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldAPI
+{
+    [ApiController]
+    [Route("/api/hello")]
+    public class HelloWorldController : ControllerBase
+    {
+        private static int _counter = 0;
+        private readonly IConfiguration _config;
+
+        public HelloWorldController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Hello()
+        {
+            return Ok(new {
+                Message = "Hello World",
+                Now = DateTime.Now,
+                Count = _counter++,
+                Name = _config["AppName"]
+            });
+        }
+    }
+}
